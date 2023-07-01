36971ff8f1ea85e7cce4ed4b57e36c714c050f47
Dmitry Kazantsev <mrduk97@gmail.com>
2023-07-01 21:42:19 +0700

    feat(map): add map navbar

---
+++ src/components/map/navigation/MapNavbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="heading">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Map-navbar'
+}
+</script>
+
+<style scoped>
+.heading {
+  margin: 0;
+  padding: 0px;
+  background-color: black;
+  color: white;
+}
+
+.heading > h1 {
+  padding: 20px;
+  margin: 0;
+}
+</style>
