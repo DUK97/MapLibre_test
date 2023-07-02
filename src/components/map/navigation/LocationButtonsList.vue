80cc399def6f50cee00dee7974d19753cab0778b
Dmitry Kazantsev <mrduk97@gmail.com>
2023-07-02 12:23:40 +0700

    feat(map): add Locations buttons list and change location button with handlers

---
+++ src/components/map/navigation/LocationButtonsList.vue
@@ -0,0 +1,53 @@
+<template>
+  <ul class="location-buttons-list">
+    <li v-for="location in locations" :key="location.id" class="location-buttons-list__item">
+      <change-location-button @changeLocation="changeMapLocation($event)" :location="location"></change-location-button>
+    </li>
+  </ul>
+
+</template>
+
+<script>
+import changeLocationButton from "@/components/utility/ChangeLocationButton";
+
+
+export default {
+
+  name: "LocationButtonsList",
+  emits: ['changeMapLocation'],
+  props:{
+    locations:{
+      type:Object,
+      required:true,
+    }
+  },
+  components: {
+    changeLocationButton
+  },
+  setup(props, context) {
+    const changeMapLocation = function (newLocation){
+      context.emit('changeMapLocation', newLocation)
+    }
+    return{changeMapLocation}
+  },
+
+}
+</script>
+
+<style scoped>
+.location-buttons-list{
+  position: absolute;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  bottom: 2%;
+  left: 1%;
+  display: flex;
+  gap: 10px;
+}
+.location-buttons-list__item{
+
+}
+
+
+</style>
---
+++ src/components/utility/ChangeLocationButton.vue
@@ -0,0 +1,49 @@
+<template>
+  <button @click="changeLocation" class="button location-button"> To {{ location.name }}</button>
+</template>
+
+<script>
+
+export default {
+  name: "ChangeLocationButton",
+  props: {
+    location: {
+      type: Object,
+      required: true,
+    }
+  },
+  emits: ['changeLocation'],
+  setup(props, context) {
+    const changeLocation = () => {
+      context.emit('changeLocation', props.location)
+    }
+    return {changeLocation}
+  },
+
+}
+
+</script>
+
+<style lang="scss" scoped>
+.location-button {
+  border: none;
+  background-color: #e1e3e9;
+  border-radius: 5px;
+  padding: 10px;
+  transition: color .3s ease, box-shadow .150ms ease;
+  box-shadow: 0 0 3px;
+  color: #2b2a28;
+  cursor: pointer;
+
+  &:hover,
+  &:focus {
+    color: #028eff;
+  }
+
+  &:active {
+    box-shadow: 0 0 0;
+  }
+}
+
+
+</style>
