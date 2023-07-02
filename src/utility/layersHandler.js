dd7d2f55e7840a22aa6e57ea2cbf121b6100857e
Dmitry Kazantsev <mrduk97@gmail.com>
2023-07-03 00:02:36 +0700

    feat(map): change size of points circle radius, change circle border color

--- src/utility/layersHandler.js
+++ src/utility/layersHandler.js
@@ -5,8 +5,6 @@
  * @param {Array} pointsLocations
  */
 export const renderMapLayer = function (mapInstance, mapSourceId, pointsLocations) {
-    console.log(mapInstance, mapSourceId);
-    console.log(pointsLocations)
     mapInstance.addSource(mapSourceId, {type: "geojson", data: pointsLocations})
     const circleColor = mapSourceId === 'pinball' ? '#F12D1E' : '#73197C';
     mapInstance.addLayer({
@@ -15,9 +13,9 @@ export const renderMapLayer = function (mapInstance, mapSourceId, pointsLocation
         'source': mapSourceId,
         paint: {
             "circle-color": circleColor,
-            "circle-radius": 7,
+            "circle-radius": 5,
             "circle-stroke-width": 1,
-            "circle-stroke-color": "#fff",
+            "circle-stroke-color": circleColor,
         }
     });
 
